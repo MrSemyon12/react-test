5a125f0fa64cbc6d743acbe0cb3799d9b92433e2
MrSemyon12 <denisenko.box19@gmail.com>
2023-03-30 18:12:11 +1000

    tests

--- autotest/test.js
+++ autotest/test.js
@@ -11,4 +11,7 @@ describe("проверка функции", () => {
   it("Milk1Sweets3Choc12Dsdw1000000kkkk0", () => {
     assert.equal(sum("Milk1Sweets3Choc12Dsdw1000000kkkk0"), 1000016);
   });
+  it("a1b2c3d4e5f6.06", () => {
+    assert.equal(sum("a1b2c3d4e5f6.06"), 21.06);
+  });
 });
