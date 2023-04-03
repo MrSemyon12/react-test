702b27f10a25e886af1d2a9c8ded6f2d6da9b783
MrSemyon12 <denisenko.box19@gmail.com>
2023-04-03 21:57:21 +1000

    index

--- js/index.js
+++ js/index.js
@@ -1,30 +1,29 @@
-const s = "Milk34Sweets3.67Choc1244.538.04Dsdw0.99kkkk0.09";
+const s = "Milk34Sweets3.67Choc1.244.538.04Dsdw0.99kkkk0.09";
 console.log(s);
 
 let regex = /[A-Za-z]+/;
-const arr = s.split(regex).slice(1);
-regex =
-  /^[1-9]+[0-9]{0,2}(.[0-9]{3})*(.(?:[0-9][1-9]|[1-9][0-9]))?$|^0(.[0-9]{2})?$/;
+  const arr = s.split(regex).slice(1);
+  if (arr.length == 0) throw Error("wrong format");
 
-const newArr = arr.map((el) => {
-  if (regex.test(el)) throw Error("wrong format");
+  regex =
+    /^[1-9][0-9]{0,2}(\.[0-9]{3})*(\.(?:[0-9][1-9]|[1-9][0-9]))?$|^0(\.[0-9]{2})?$/;
 
-  if (el.match(regex) === null) {
-  }
+  const newArr = arr.map((el) => {
+    if (!regex.test(el)) throw Error("wrong format");
 
-  if (el.length <= 4) {
-    return Number(el);
-  }
+    if (el.length <= 4) {
+      return Number(el);
+    }
 
-  if (el[el.length - 3] === ".") {
-    return Number(
-      el.slice(0, el.length - 3).replace(".", "") +
-        "." +
-        el.slice(el.length - 2)
-    );
-  }
-  return Number(el.replace(".", ""));
-});
+    if (el[el.length - 3] === ".") {
+      return Number(
+        el.slice(0, el.length - 3).replaceAll(".", "") +
+          "." +
+          el.slice(el.length - 2)
+      );
+    }
+    return Number(el.replaceAll(".", ""));
+  });
 
 console.log(arr);
 console.log(newArr.reduce((ac, val) => ac + val, 0));
