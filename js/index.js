d90cb49e00bcbe240243feaaa09e20df2fbd2b54
MrSemyon12 <denisenko.box19@gmail.com>
2023-04-05 18:43:03 +1000

    smth

--- js/index.js
+++ js/index.js
@@ -1,29 +1,19 @@
-const s = "Milk34Sweets3.67Choc1.244.538.04Dsdw0.99kkkk0";
-console.log(s);
+async function getUserName(id) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  ).catch((e) => console.log(e.message));
+  const json = await response.json();
+  return json.name;
+}
 
-let regex = /[A-Za-z]+/;
-const arr = s.split(regex).slice(1);
-if (arr.length == 0) throw Error("wrong format");
+async function getPostBodyAndUserName(id) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  ).catch((e) => console.log(e.message));
 
-regex =
-  /^[1-9][0-9]{0,2}(\.[0-9]{3})*(\.(?:[0-9][1-9]|[1-9][0-9]))?$|^0(\.(?:[0-9][1-9]|[1-9][0-9]))?$/;
+  const json = await response.json();
+  const username = await getUserName(json.userId);
+  console.log(`${json.body}\n\n${username}`);
+}
 
-const newArr = arr.map((el) => {
-  if (!regex.test(el)) throw Error("wrong format");
-
-  if (el.length <= 4) {
-    return Number(el);
-  }
-
-  if (el[el.length - 3] === ".") {
-    return Number(
-      el.slice(0, el.length - 3).replaceAll(".", "") +
-        "." +
-        el.slice(el.length - 2)
-    );
-  }
-  return Number(el.replaceAll(".", ""));
-});
-
-console.log(arr);
-console.log(newArr.reduce((ac, val) => ac + val, 0));
+getPostBodyAndUserName(10);
