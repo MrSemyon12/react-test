e8d64291223bdff171ab80d2041afebf614ec790
MrSemyon12 <denisenko.box19@gmail.com>
2023-04-08 19:28:45 +1000

    props

--- myapp/src/components/App.jsx
+++ myapp/src/components/App.jsx
@@ -1,7 +1,12 @@
-import About from "./About";
+import Post from "./Post";
+import "../css/App.css";
 
 function App() {
-  return <About />;
+  return (
+    <div className="App">
+      <Post post={{ id: 1, title: "Java", body: "sadDadwfafaf" }} />
+    </div>
+  );
 }
 
 export default App;
